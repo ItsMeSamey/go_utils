1f8d132fea59e64fdc47bfd5cd1b3474d7522846
ItsMeSamey <sameychain5041@gmail.com>
2024-11-12 13:17:27 +0530

    added optional type

---
+++ Optional.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+  "github.com/goccy/go-json"
+)
+
+type Optional[T any] struct {
+  Val T
+  Exists bool
+}
+
+func (self Optional[T]) MarshalJSON() ([]byte, error) {
+  if self.Exists {
+    return json.Marshal(self.Val)
+  } else {
+    return json.Marshal(nil)
+  }
+}
+
+func (self *Optional[T]) UnmarshalJSON(data []byte) error {
+  self.Exists = !(len(data) == 4 && B2S(data) == "null")
+
+  return json.Unmarshal(data, &self.Val)
+}
+
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ItsMeSamey/go_utils
 
 go 1.23.2
+
+require github.com/goccy/go-json v0.10.3
